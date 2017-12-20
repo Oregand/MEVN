c14c47da69478fbb4a568aba7539731f08eb2c32
David O'Regan <doregan@vsware.ie>
2017-12-20 18:52:40 +0000

    [#2] Update local web config for better local development

--- web/webapp/config/dev.env.js
+++ web/webapp/config/dev.env.js
@@ -4,5 +4,5 @@ const prodEnv = require('./prod.env');
 
 module.exports = merge(prodEnv, {
   NODE_ENV: '"development"',
-  BASE_URL: '"http://localhost:8082"',
+  BASE_URL: '"http://localhost:3000"',
 });

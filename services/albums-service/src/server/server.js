a1277a150f0dca56cd662ba511d3562ccbacb043
David O'Regan <doregan@vsware.ie>
2017-12-20 21:53:36 +0000

    [#2] Update local web config for better local development

--- services/albums-service/src/server/server.js
+++ services/albums-service/src/server/server.js
@@ -2,6 +2,9 @@ const express = require('express');
 const morgan = require('morgan');
 const helmet = require('helmet');
 const api = require('../api/albums');
+const jwt = require('express-jwt');
+const jwks = require('jwks-rsa');
+const history = require('connect-history-api-fallback');
 
 const start = (options) => {
   return new Promise((resolve, reject) => {
@@ -14,13 +17,26 @@ const start = (options) => {
 
     const app = express();
     app.use(morgan('dev'));
+    //app.use(history());
     app.use(helmet());
     app.use((err, req, res, next) => {
       reject(new Error('Something went wrong!, err:' + err));
-      res.status(500).send('Something went wrong!');
+      res.status(500).send('Something went wrongnpm !');
     });
 
-    api(app, options);
+    const authCheck = jwt({
+      secret: jwks.expressJwtSecret({
+        cache: true,
+        rateLimit: true,
+        jwksRequestsPerMinute: 5,
+        jwksUri: 'https://david-personal.eu.auth0.com/.well-known/jwks.json',
+      }),
+      audience: 'http://mvenstarter.com',
+      issuer: 'https://david-personal.eu.auth0.com/',
+      algorithms: ['RS256'],
+    });
+
+    api(app, options, authCheck);
 
     const server = app.listen(options.port, () => resolve(server));
   });
